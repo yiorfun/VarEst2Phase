4d0754eb90d3f2099d6df4225fdc7a0bf82afc2d
Evan Yeung <yiorfun@gmail.com>
2022-08-30 15:28:53 -0400

    add: the first written function

---
+++ R/FirstFunctions.R
@@ -0,0 +1,14 @@
+#' This is the title for our FirstFunctions, Returns the sum of two numbers
+#'
+#' @param a Explain the parameter a, say, Number One
+#' @param b Explain the parameter b, say, Number Two
+#'
+#' @return Explain what is the result, say, Sum of two numbers
+#' @export
+#'
+#' @examples
+#' library(VarEst2Phase)
+#' FirstFunctions(1, 2)
+FirstFunctions <- function(a, b) {
+  a+b
+}
