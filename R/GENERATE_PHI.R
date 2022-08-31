f843f8622bef2c0ed82611551dfae4f6abd59449
Evan Yeung <yiorfun@gmail.com>
2022-08-31 18:45:45 -0400

    modify GENERATE_PHI

--- R/GENERATE_PHI.R
+++ R/GENERATE_PHI.R
@@ -1,17 +1,48 @@
-#' Calculate the values of the polynomial spline at a given vector
+#' Calculate the spline basis matrix and the values of a linear combination
+#' of B-spline biasis function at a given vector
 #'
-#' @param x The predictor variable (or vector). Missing values are allowed.
-#' @param knot The internal breakpoints (i.e., the breakpoints without two ending points) that define the spline. The default is `NULL`, which results in a basis for the ordinary polynomial regression. Typical values are the mean or median for one knot, quantiles for more knots. See also `Boundary.knots`.
-#' @param degree A non-negative integer degree of the piece-wise polynomial. The default value is 3 for cubic splines. Zero degree is allowed for this function, which is the only difference compared with `bs` function in package `splines`. Note that the order of a polynomial equals to the degree plus one.
-#' @param boundary A 2-dimensional boundary points at which to anchor the B-spline basis. By default, they are the rangle of the non-`NA` data. If both `knots` and `Boundary.knots` are supplied, the basis parameters do not depend on `x`. Data can extend beyound `Boundary.knots`.
-#' @param coeff_spline The coefficients variables that are evaluated corresponding to the B-spline basis matrix included an intercept. The length of `coeff_spline` equals `df` or `degre + length(knots) + 1` (due to the presence of an intercept).
+#' @param x The predictor vector. Missing values are allowed.
 #'
-#' @return A matrix of dimension `length(x)` by `df = degree + length(knots)` (plus one if the intercept is included). Attributes that correspond to the arguments specificed are returned for useage of other functions in this package.
+#' @param knot The internal breakpoints (i.e., the breakpoints without two
+#'     ending points) that define the spline. The default is `NULL`, which
+#'     results in a basis for the ordinary polynomial regression. Typical
+#'     values are the mean or median for one knot, quantiles for more knots.
+#'     See also `Boundary.knots`.
+#'
+#' @param degree A non-negative integer degree of the piece-wise polynomial.
+#'     The default value is 3 for cubic splines. Zero degree is allowed for
+#'     this function, which is the only difference compared with `bs`
+#'     function in package `splines`. Note that the order of a polynomial
+#'     equals to the degree plus one.
+#'
+#' @param boundary A 2-dimensional boundary points at which to anchor the
+#'     B-spline basis. By default, they are the range of the non-`NA` data.
+#'     If both `knot` and `boundary` are supplied, the basis parameters do
+#'     not depend on `x`. Data can extend beyond `boundary`.
+#'
+#' @param coeff_spline A coefficients vector that are evaluated corresponding
+#'     to the B-spline basis matrix included an intercept. The length of
+#'     `coeff_spline` equals `df` or `degree + length(knot) + 1`
+#'     (due to the presence of an intercept).
+#'
+#' @return A matrix of dimension `length(x)` by `df = degree + length(knot)`
+#'     (plus one if the intercept is included) and a vector that the product
+#'     of this matrix and the vector `coeff_spline`. Attributes that
+#'     correspond to the arguments specified are returned for usage of other
+#'     functions in this package.
+#'
 #' @export
+#'
+#' @importFrom splines2 bSpline
 #'
 #' @examples
 #' library(VarEst2Phase)
-#' GENERATE_PHI(x = c(1, 2, 3), knot = c(0.25, 0.75), degree = 2, boundary = c(0, 1), coeff_spline = c(0.1, 0.2, 0.3))
+#' GENERATE_PHI(x = c(0.1, 0.2, 0.3),
+#'           knot = c(0.25, 0.75),
+#'         degree = 2,
+#'       boundary = c(0, 1),
+#'   coeff_spline = c(0.1, 0.2, 0.3, 0.4, 0.5))
+
 GENERATE_PHI <- function(x, knot, degree, boundary, coeff_spline) {
   phi_matrix <- bSpline(
     x = x,
